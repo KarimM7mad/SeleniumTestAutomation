44c28f19de8b33d57455ae46b9f7b07155995ade
KarimM7mad <KarimMohamed95@hotmail.com>
2019-04-16 05:45:21 +0200

    commit 1

---
+++ projectCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projectCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
